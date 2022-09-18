e90d929d03062328c14b2c5b5e7c606dcf579065
Ben Stein <ben.s.stein@gmail.com>
2022-09-18 12:50:58 -0400

    Set snackbar position in wasm globally.

--- src/MorphWasm/Program.cs
+++ src/MorphWasm/Program.cs
@@ -10,7 +10,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // for MudBlazor
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = "mud-snackbar-location-top-center";
+});
 
 // add shared services and platform-specific services
 builder.Services.AddMorphSharedServices(platformServices =>
